5cebc076cfc7b10fa202c5320941c8b9aa7ae5ef
congcq <aiskai2007@gmail.com>
2025-05-31 21:30:02 +0700

    Remove tinygl4angle

--- Natives/egl_bridge.m
+++ Natives/egl_bridge.m
@@ -57,10 +57,7 @@ int pojavInitOpenGL() {
         renderer = @ RENDERER_NAME_GL4ES;
         setenv("POJAV_RENDERER", renderer.UTF8String, 1);
         set_gl_bridge_tbl();
-    } else if ([renderer isEqualToString:@ RENDERER_NAME_MTL_ANGLE]) {
-        set_gl_bridge_tbl();
     } else if ([renderer isEqualToString:@ RENDERER_NAME_MOBILEGLUES]) {
-        setenv("POJAV_RENDERER", renderer.UTF8String, 1);
         set_gl_bridge_tbl();
     } else if ([renderer hasPrefix:@"libOSMesa"]) {
         setenv("GALLIUM_DRIVER","zink",1);
@@ -86,8 +83,8 @@ void pojavSetWindowHint(int hint, int value) {
                 break;
             // case 4: use Zink?
             default:
-                setenv("POJAV_RENDERER", RENDERER_NAME_MTL_ANGLE, 1);
-                JNI_LWJGL_changeRenderer(RENDERER_NAME_MTL_ANGLE);
+                setenv("POJAV_RENDERER", RENDERER_NAME_MOBILEGLUES, 1);
+                JNI_LWJGL_changeRenderer(RENDERER_NAME_MOBILEGLUES);
                 break;
         }
     }
